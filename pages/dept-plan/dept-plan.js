0b5db7a83921efd758013e9d851e936654e5f3d4
shanguibot <1164535481@qq.com>
2026-02-11 22:52:37 +0800

    完成所有页面的界面更新
    - 更新学生评语页面（支持批量生成、Excel导出）
    - 更新班主任工作总结页面（9个选填字段）
    - 更新班主任工作计划页面（9个选填字段）
    - 更新科室工作总结页面（关键事件、成绩、问题、建议）
    - 更新科室工作计划页面（活动、目标、措施、时间）
    - 新增反馈页面（有需求，直接提）
    - 统一样式风格，添加必填项标识
    - 所有页面支持4种写作风格和3种长度选择

--- pages/dept-plan/dept-plan.js
+++ pages/dept-plan/dept-plan.js
@@ -7,8 +7,6 @@ Page({
     formData: {
       deptName: '',
       semester: '',
-      length: '标准版',
-      style: '传统公文体',
       activities: '',
       goals: '',
       measures: '',
@@ -29,19 +27,14 @@ Page({
   },
 
   onLengthChange(e) {
-    const index = e.detail.value
-    const length = this.data.lengthOptions[index].split(' ')[0]
     this.setData({
-      lengthIndex: index,
-      'formData.length': length
+      lengthIndex: e.detail.value
     })
   },
 
   onStyleChange(e) {
-    const index = e.detail.value
     this.setData({
-      styleIndex: index,
-      'formData.style': this.data.styleOptions[index]
+      styleIndex: e.detail.value
     })
   },
 
@@ -88,8 +81,8 @@ Page({
     setTimeout(() => {
       wx.hideLoading()
       wx.navigateTo({
-        url: '/pages/preview/preview?type=dept-plan&data=' + JSON.stringify(this.data.formData)
+        url: '/pages/preview/preview?type=dept-plan'
       })
-    }, 1500)
+    }, 2000)
   }
 })
